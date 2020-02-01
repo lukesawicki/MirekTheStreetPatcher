f9283513de9fab8f581c8a2f9d6a8c7277c9e300
lukesawicki <lukaszsa1@gmail.com>
2020-02-01 18:56:51 +0100

    Car spawners and obstacles
    Car spawners and obstacles

---
+++ Assets/Script/CarController.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+
+    [SerializeField] float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    public BoxCollider2D collider2D;
+    Vector2 movement;
+    public float movementX;
+    public float movementY;
+    //public bool isDirectionReadyToChange = true;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        collider2D = GetComponent<BoxCollider2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Hole"))
+        {
+            if (collision.gameObject.CompareTag("DirectionChangerRight"))
+            {
+                changeDirectionToTheRight();
+            }
+            else
+            {
+                if (collision.gameObject.CompareTag("DirectionChangerLeft"))
+                {
+                    changeDirectionToTheLeft();
+                }
+            }
+        }
+        else
+        {
+
+        }
+    }
+
+    public void changeDirectionToTheRight()
+    {
+        if (movementX + movementY == 1) // is up or right
+        {
+            if (movementX == 1) // is right
+            {
+                movementX = 0;
+                movementY = -1;
+            }
+            else // is up
+            {
+                movementX = 1;
+                movementY = 0;
+            }
+        }
+        else
+        {
+            if (movementX == 0) // is down
+            {
+                movementX = -1;
+                movementY = 0;
+            }
+            else // is left
+            {
+                movementX = 0;
+                movementY = 1;
+            }
+        }
+    }
+
+    public void changeDirectionToTheLeft()
+    {
+        if (movementX + movementY == 1) // is up or right
+        {
+            if (movementX == 1) // is right
+            {
+                movementX = 0;
+                movementY = 1;
+            }
+            else // is up
+            {
+                movementX = -1;
+                movementY = 0;
+            }
+        }
+        else
+        {
+            if (movementX == 0) // is down
+            {
+                movementX = 1;
+                movementY = 0;
+            }
+            else // is left
+            {
+                movementX = 0;
+                movementY = -1;
+            }
+        }
+    }
+
+    public void stopCar()
+    {
+        movementX = 0;
+        movementY = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = movementX;
+        movement.y = movementY;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/Script/CarSpawnerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSpawnerScript : MonoBehaviour
+{
+    public GameObject prefabToSpawn;
+    public bool stopSpawning = false;
+    public float spawnTime;
+    public float spawnDelay;
+
+    public int carsCounter = 0;
+    public int maxCarsOnLevel = 0;
+
+    public void SpawnObject()
+    {
+        Instantiate(prefabToSpawn, transform.position, transform.rotation);
+        if (stopSpawning)
+        {
+            CancelInvoke("SpawnObject");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
